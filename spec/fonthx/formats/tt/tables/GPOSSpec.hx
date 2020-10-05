58081f300f1b0eac28a13f740add93f620a7aa43
rob <vc@robmeek.com>
2020-10-05 21:49:26 +0200

    WIP

--- spec/fonthx/formats/tt/tables/GPOSSpec.hx
+++ spec/fonthx/formats/tt/tables/GPOSSpec.hx
@@ -1,12 +1,21 @@
 package fonthx.formats.tt.tables;
 
+import fonthx.examples.pixelfonter.PixelFont;
+import fonthx.formats.tt.writers.TrueTypeFileWriter;
 import fonthx.formats.tt.tables.opentype.GPOSTable;
 using buddy.Should;
 
+@include
 class GPOSSpec extends buddy.BuddySuite {
     public function new() {
         describe("GPOS", function() {
             var gpos = new GPOSTable();
+            var font = new PixelFont('test-font', 1000, 10);
+
+            font.prepareForExport();
+            var bytes = TrueTypeBuilder.build(font, TrueType);
+            trace(bytes.sub(0, 4));
+
         });
     }
 }
